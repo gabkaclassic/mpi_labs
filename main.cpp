e5b079b703b8f5a75e7003c918cc3a25903800d9
Errodion <kuzminrd44@gmail.com>
2023-10-01 11:15:05 +0300

    lab_3

--- main.cpp
+++ main.cpp
@@ -9,25 +9,43 @@ void star(int N, int M) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size != N + 1) {
-        std::cerr << "Error: expected " << N + 1 << " processes, but got " << size << std::endl;
-        MPI_Abort(MPI_COMM_WORLD, 1);
+    // Создаем массив данных, который будет разделен между процессами
+    std::vector<int> data(N * M);
+    if (rank == 0) {
+        for (int i = 0; i < N * M; i++) {
+            data[i] = i;
+        }
     }
 
-    std::vector<int> messages(M);
+    // Определяем размеры блоков для каждого процесса
+    std::vector<int> counts(size);
+    std::vector<int> displs(size);
+    int block_size = N * M / size;
+    for (int i = 0; i < size; i++) {
+        counts[i] = block_size;
+        displs[i] = i * block_size;
+    }
+    counts[size - 1] = N * M - (size - 1) * block_size;
+
+    // Разделяем массив данных между процессами
+    std::vector<int> local_data(block_size);
+    MPI_Scatterv(data.data(), counts.data(), displs.data(), MPI_INT,
+                 local_data.data(), block_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Обрабатываем блок данных
+    for (int i = 0; i < block_size; i++) {
+        local_data[i] = rank;
+    }
+
+    // Отправляем результаты обратно процессу с рангом 0
+    std::vector<int> result(N * M);
+    MPI_Gatherv(local_data.data(), block_size, MPI_INT,
+                result.data(), counts.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        for (int i = 0; i < M; i++) {
-            messages[i] = i;
-        }
-        MPI_Bcast(messages.data(), M, MPI_INT, 0, MPI_COMM_WORLD);
-
-    }
-
-
-    if (rank != 0) {
-        for (int i = 0; i < M; i++) {
-            std::cout << "Process " << rank << " received message " << messages[i] << std::endl;
+        // Выводим результаты
+        for (int i = 0; i < N * M; i++) {
+            std::cout << "Process 0 received message " << result[i] << std::endl;
         }
     }
 
@@ -43,6 +61,6 @@ int main(int argc, char** argv) {
     s2 >> M;
 
     MPI_Init(&argc, &argv);
-    star(N-1, M);
+    star(N, M);
     return 0;
 }
