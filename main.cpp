55713ec184c89dde5794fe88609befc47c19d0b7
Errodion <kuzminrd44@gmail.com>
2023-10-01 10:10:14 +0300

    lab_2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <mpi.h>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 void star(int N, int M) {
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     if (size != N + 1) {
         std::cerr << "Error: expected " << N + 1 << " processes, but got " << size << std::endl;
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    for (int i = 0; i < M; i++) {
-        if (rank == 0) {
-            int message = i;
-            for (int j = 1; j < size; j++) {
-                MPI_Send(&message, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
-            }
-        } else {
-            int message;
-            MPI_Recv(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            std::cout << "Process " << rank << " received message " << message << std::endl;
-            MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    std::vector<int> messages(M);
+
+    if (rank == 0) {
+        for (int i = 0; i < M; i++) {
+            messages[i] = i;
+        }
+        MPI_Bcast(messages.data(), M, MPI_INT, 0, MPI_COMM_WORLD);
+
+    }
+
+
+    if (rank != 0) {
+        for (int i = 0; i < M; i++) {
+            std::cout << "Process " << rank << " received message " << messages[i] << std::endl;
         }
     }
 
